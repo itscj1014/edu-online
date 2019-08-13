6970a2361649764336e97b499e2cddefb450ff33
scj <18255465990@163.com>
2019-08-13 15:22:15 +0800

    教师列表分页条件查询 by scj

--- guli-microservice-edu/src/main/java/com/guli/edu/controller/admin/TeacherAdminController.java
+++ guli-microservice-edu/src/main/java/com/guli/edu/controller/admin/TeacherAdminController.java
@@ -1,8 +1,11 @@
 package com.guli.edu.controller.admin;
 
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.guli.common.vo.R;
 import com.guli.edu.entity.Teacher;
+import com.guli.edu.query.TeacherQuery;
 import com.guli.edu.service.TeacherService;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +32,15 @@ public class TeacherAdminController {
     public R removeTeacherById(@PathVariable("id") String id) {
         teacherService.removeById(id);
         return R.ok();
+    }
+
+    @GetMapping("{page}/{limit}")
+    public R tearchPage(@PathVariable("page")Integer page, @PathVariable("limit") Integer limit, TeacherQuery teacherQuery) {
+        Page<Teacher> pageParm = new Page<>(page, limit);
+        IPage<Teacher> teacherIPage = teacherService.pageQuery(pageParm,teacherQuery);
+        List<Teacher> records = teacherIPage.getRecords();
+        long total = teacherIPage.getTotal();
+        return R.ok().data("total", total).data("rows", records);
     }
 
 }
--- guli-microservice-edu/src/main/java/com/guli/edu/service/impl/TeacherServiceImpl.java
+++ guli-microservice-edu/src/main/java/com/guli/edu/service/impl/TeacherServiceImpl.java
@@ -1,10 +1,17 @@
 package com.guli.edu.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.guli.edu.entity.Teacher;
 import com.guli.edu.mapper.TeacherMapper;
+import com.guli.edu.query.TeacherQuery;
 import com.guli.edu.service.TeacherService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.nio.channels.NonReadableChannelException;
 
 /**
  * <p>
@@ -17,4 +24,41 @@ import org.springframework.stereotype.Service;
 @Service
 public class TeacherServiceImpl extends ServiceImpl<TeacherMapper, Teacher> implements TeacherService {
 
+    @Override
+    public IPage<Teacher> pageQuery(Page<Teacher> pageParm, TeacherQuery teacherQuery) {
+
+        IPage<Teacher> teacherIPage;
+        QueryWrapper<Teacher> queryWrapper = new QueryWrapper<>();
+        queryWrapper.orderByAsc("sort");
+
+        if (teacherQuery == null) {
+            teacherIPage = baseMapper.selectPage(pageParm, null);
+            return teacherIPage;
+        }
+
+        String name = teacherQuery.getName();
+        Integer level = teacherQuery.getLevel();
+        String begin = teacherQuery.getBegin();
+        String end = teacherQuery.getEnd();
+
+        if (!StringUtils.isEmpty(name)) {
+            queryWrapper.like("name", name);
+        }
+
+        if (!StringUtils.isEmpty(level)) {
+            queryWrapper.eq("level", level);
+        }
+
+        if (!StringUtils.isEmpty(begin)) {
+            queryWrapper.gt("gmt_create", begin);
+        }
+
+        if (!StringUtils.isEmpty(end)) {
+            queryWrapper.gt("gmt_create", end);
+        }
+
+        teacherIPage = baseMapper.selectPage(pageParm, queryWrapper);
+
+        return teacherIPage;
+    }
 }
