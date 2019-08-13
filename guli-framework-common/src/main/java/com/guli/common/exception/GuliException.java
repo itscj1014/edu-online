8789541ba6840aaa332d0520c5668f1aeebed3b0
scj <18255465990@163.com>
2019-08-13 16:35:57 +0800

    统一全局异常处理 by scj

---
+++ guli-framework-common/src/main/java/com/guli/common/exception/GuliException.java
@@ -0,0 +1,29 @@
+package com.guli.common.exception;
+
+import com.guli.common.constants.ResultCodeEnum;
+import io.swagger.annotations.ApiModel;
+import lombok.Data;
+
+@Data
+@ApiModel("全局异常")
+public class GuliException extends RuntimeException {
+
+    private Integer code;
+
+    /**
+     * 接收状态码和消息
+     */
+    public GuliException(Integer code, String message) {
+        super(message);
+        this.code = code;
+    }
+
+    /**
+     * 接收枚举类型
+     */
+    public GuliException(ResultCodeEnum resultCodeEnum) {
+        super(resultCodeEnum.getMessage());
+        this.code = resultCodeEnum.getCode();
+    }
+
+}
