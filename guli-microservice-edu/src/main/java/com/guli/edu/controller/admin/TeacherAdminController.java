f769343a199d148757f730587f36483a0757f15a
scj <18255465990@163.com>
2019-08-13 13:42:52 +0800

    添加跨域请求设置 by scj

--- guli-microservice-edu/src/main/java/com/guli/edu/controller/admin/TeacherAdminController.java
+++ guli-microservice-edu/src/main/java/com/guli/edu/controller/admin/TeacherAdminController.java
@@ -3,13 +3,13 @@ package com.guli.edu.controller.admin;
 
 import com.guli.edu.entity.Teacher;
 import com.guli.edu.service.TeacherService;
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(value = "教师管理", description = "教师管理")
 @RestController
 @RequestMapping("/admin/edu/teacher")
 public class TeacherAdminController {
@@ -22,6 +22,12 @@ public class TeacherAdminController {
         System.out.println("请求进来了");
         List<Teacher> teacherList = teacherService.list(null);
         return teacherList;
+    }
+
+    @DeleteMapping("{id}")
+    public boolean removeTeacherById(@PathVariable("id") String id) {
+        boolean result = teacherService.removeById(id);
+        return result;
     }
 
 }
